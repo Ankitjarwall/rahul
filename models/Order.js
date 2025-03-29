2ed1de345965fbc42141e52224dfeb4b2e9f386a
Ankit <ankitmeena9783226195@gmail.com>
2025-03-29 05:48:11 +0530

    revert

--- models/Order.js
+++ models/Order.js
@@ -20,18 +20,4 @@ const orderSchema = new mongoose.Schema({
     comments: String,
 }, { timestamps: true });
 
-// Pre-save middleware to capitalize string fields
-orderSchema.pre('save', function (next) {
-    if (this.shopName) this.shopName = this.shopName.toUpperCase();
-    if (this.address) this.address = this.address.toUpperCase();
-    if (this.town) this.town = this.town.toUpperCase();
-    if (this.contact) this.contact = this.contact.toUpperCase();
-    if (this.productDetails) this.productDetails = this.productDetails.toUpperCase();
-    if (this.weight) this.weight = this.weight.toUpperCase();
-    if (this.rate) this.rate = this.rate.toUpperCase();
-    if (this.paymentMethod) this.paymentMethod = this.paymentMethod.toUpperCase();
-    if (this.comments) this.comments = this.comments.toUpperCase();
-    next();
-});
-
 module.exports = mongoose.model('Order', orderSchema);
