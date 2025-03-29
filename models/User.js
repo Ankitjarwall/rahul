4a2de4d9c02ae59fd213cd15bad937fc6ae387b8
Ankit <ankitmeena9783226195@gmail.com>
2025-03-29 06:26:39 +0530

    fixed the db issue

--- models/User.js
+++ models/User.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
 
 const userSchema = new mongoose.Schema({
-    userId: { type: String, required: true, unique: true },
+    userId: { type: String, required: true, unique: true, default: uuidv4 },
     shopName: String,
     address: String,
     town: String,
