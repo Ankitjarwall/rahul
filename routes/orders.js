f93e2ea11a14b3c5d1f85e10b39fc490766b97a4
Ankit <ankitmeena9783226195@gmail.com>
2025-04-04 05:50:10 +0530

    Update orders.js

--- routes/orders.js
+++ routes/orders.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Order = require('../models/Order');
-const pdf = require('html-pdf');
 const fs = require('fs');
 const path = require('path');
 const puppeteer = require('puppeteer');
