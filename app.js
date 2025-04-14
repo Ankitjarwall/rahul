b0e349e8a6c6542b7b19cb142e5c4fc6f6b661dd
Ankit <ankitmeena9783226195@gmail.com>
2025-04-15 00:26:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true }));
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
